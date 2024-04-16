f6f40032159b328795999c9dabfaf7a2ec0e4da5
kaggle68 <kaggle68@gmail.com>
2024-04-16 20:50:51 +0900

    2 cha wan seong

--- lib/presentation/exchange_screen.dart
+++ lib/presentation/exchange_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:http/http.dart' as http;
 
 import '../data/data_source/exchange_data_source.dart';
@@ -52,15 +53,63 @@ class _ExchangeScreenState extends State<ExchangeScreen> {
     }
   }
 
+  Widget _buildDropdown(
+      String? value, List<String> items, ValueChanged<String?> onChanged) {
+    return InputDecorator(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        labelText: '국가 선택',
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          isExpanded: true,
+          value: value,
+          onChanged: onChanged,
+          items: items.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildResultBox() {
+    return InputDecorator(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      ),
+      child: Text(
+        _exchangeRateResult != null
+            ? _exchangeRateResult!.toStringAsFixed(2)
+            : '결과가 결과',
+        style: TextStyle(fontSize: 16, color: Colors.black.withOpacity(0.7)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Currency Exchange')),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          '환율이 환율',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            const SizedBox(
+              height: 100,
+            ),
             Row(
               children: [
                 Expanded(
@@ -68,58 +117,40 @@ class _ExchangeScreenState extends State<ExchangeScreen> {
                     controller: _amountController,
                     keyboardType: TextInputType.number,
                     decoration: const InputDecoration(
-                      labelText: 'Amount',
-                      hintText: 'Enter amount to convert',
+                      labelText: '머니가 Money',
+                      hintText: 'Money is 머니',
+                      border: OutlineInputBorder(),
                     ),
                   ),
                 ),
                 const SizedBox(width: 20),
                 Expanded(
-                  child: DropdownButton<String>(
-                    isExpanded: true,
-                    value: _baseCurrency,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _baseCurrency = newValue;
-                      });
-                      _updateExchangeRate();
-                    },
-                    items: _currencies
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
+                  child: _buildDropdown(_baseCurrency, _currencies, (newValue) {
+                    setState(() {
+                      _baseCurrency = newValue;
+                    });
+                    _updateExchangeRate();
+                  }),
                 ),
               ],
             ),
+            const SizedBox(height: 20), // Vertical space between the rows
             Row(
               children: [
                 Expanded(
-                  child: Text(_exchangeRateResult != null
-                      ? 'Converted: $_exchangeRateResult $_targetCurrency'
-                      : 'Enter amount and select currencies'),
+                  child: _buildResultBox(),
                 ),
+                const SizedBox(
+                    width:
+                        20), // Horizontal space between the result box and the second dropdown
                 Expanded(
-                  child: DropdownButton<String>(
-                    isExpanded: true,
-                    value: _targetCurrency,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _targetCurrency = newValue;
-                      });
-                      _updateExchangeRate();
-                    },
-                    items: _currencies
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
+                  child:
+                      _buildDropdown(_targetCurrency, _currencies, (newValue) {
+                    setState(() {
+                      _targetCurrency = newValue;
+                    });
+                    _updateExchangeRate();
+                  }),
                 ),
               ],
             ),
