5e292c4db8204f3a6a3b9a3316843bcc242383f3
kaggle68 <kaggle68@gmail.com>
2024-04-16 19:33:57 +0900

    1차 완성

--- lib/data/repository/exchange_repository_impl.dart
+++ lib/data/repository/exchange_repository_impl.dart
@@ -11,30 +11,30 @@ class ExchangeRepositoryImpl implements ExchangeRepository {
 
   @override
   Future<ExchangeModel> getExchanges(String query) async {
-    try {
-      // 데이터 소스로부터 ExchangeDto를 가져옵니다.
-      final dto = await _exchangeDataSource.getExchangeResult(query);
-      if (dto == null) {
-        // dto가 null인 경우, 기본 ConversionRates를 설정합니다.
-        return ExchangeModel(
-          conversionRates: ConversionRates.fromMap({
-            'USD': 1.0, // USD를 기준으로 기본값 설정
-            // 다른 통화에 대한 기본값도 필요에 따라 추가할 수 있음
-          }),
-          timeLastUpdateUtc: 'No data available',
-          baseCode: 'USD',
-        );
-      } else {
-        // dto가 null이 아닌 경우, dto를 사용하여 ExchangeModel을 생성합니다.
-        return ExchangeModel(
-            conversionRates: ConversionRates.fromMap(
-                dto.conversionRates as Map<String, num>),
-            timeLastUpdateUtc: dto.timeLastUpdateUtc,
-            baseCode: dto.baseCode);
-      }
-    } catch (e) {
-      // 오류 처리: 로그 출력, 오류 전파 등
-      throw Exception('Failed to fetch exchange data');
+// try {
+// 데이터 소스로부터 ExchangeDto를 가져옵니다.
+    final dto = await _exchangeDataSource.getExchangeResult(query);
+
+    if (dto == null) {
+// dto가 null인 경우, 기본 ConversionRates를 설정합니다.
+      return ExchangeModel(
+        conversionRates: ConversionRates.fromMap({
+          'USD': 1.0, // USD를 기준으로 기본값 설정
+// 다른 통화에 대한 기본값도 필요에 따라 추가할 수 있음
+        }),
+        timeLastUpdateUtc: 'No data available',
+        baseCode: 'USD',
+      );
+    } else {
+// dto가 null이 아닌 경우, dto를 사용하여 ExchangeModel을 생성합니다.
+      return ExchangeModel(
+          conversionRates: ConversionRates.fromJson(dto.toJson()),
+          timeLastUpdateUtc: dto.timeLastUpdateUtc,
+          baseCode: dto.baseCode);
     }
+// } catch (e) {
+// // 오류 처리: 로그 출력, 오류 전파 등
+// throw Exception('Failed to fetch exchange data');
+// }
   }
 }
--- lib/presentation/exchange_screen.dart
+++ lib/presentation/exchange_screen.dart
@@ -1,99 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../data/data_source/exchange_data_source.dart';
 
 class ExchangeScreen extends StatefulWidget {
-  const ExchangeScreen({Key? key});
+  const ExchangeScreen({super.key});
 
   @override
   State<ExchangeScreen> createState() => _ExchangeScreenState();
 }
 
 class _ExchangeScreenState extends State<ExchangeScreen> {
-  String _selectedCurrency = 'USD'; // 초기 선택 통화 설정
+  final _amountController = TextEditingController();
+  String? _baseCurrency;
+  String? _targetCurrency;
+  double? _exchangeRateResult;
+  final List<String> _currencies = [
+    "USD",
+    "EUR",
+    "JPY",
+    "KRW"
+  ]; // Example currencies
+  ExchangeDataSource? _dataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    _dataSource = ExchangeDataSource(client: http.Client());
+    _baseCurrency = _currencies.first;
+    _targetCurrency = _currencies.last;
+    _amountController.addListener(_updateExchangeRate);
+  }
+
+  void _updateExchangeRate() async {
+    if (_baseCurrency != null &&
+        _targetCurrency != null &&
+        _amountController.text.isNotEmpty) {
+      try {
+        final exchangeDto =
+            await _dataSource!.getExchangeResult(_baseCurrency!);
+        final rate = exchangeDto!.conversionRates.toJson()[_targetCurrency];
+        double amount = double.tryParse(_amountController.text) ?? 0.0;
+        setState(() {
+          _exchangeRateResult = amount * (rate ?? 0.0);
+        });
+      } catch (e) {
+        setState(() {
+          _exchangeRateResult = null;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '환율 검색',
-          style: TextStyle(fontWeight: FontWeight.w600),
+      appBar: AppBar(title: const Text('Currency Exchange')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Amount',
+                      hintText: 'Enter amount to convert',
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: DropdownButton<String>(
+                    isExpanded: true,
+                    value: _baseCurrency,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _baseCurrency = newValue;
+                      });
+                      _updateExchangeRate();
+                    },
+                    items: _currencies
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(_exchangeRateResult != null
+                      ? 'Converted: $_exchangeRateResult $_targetCurrency'
+                      : 'Enter amount and select currencies'),
+                ),
+                Expanded(
+                  child: DropdownButton<String>(
+                    isExpanded: true,
+                    value: _targetCurrency,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _targetCurrency = newValue;
+                      });
+                      _updateExchangeRate();
+                    },
+                    items: _currencies
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 180,
-                height: 50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: '검색어',
-                  ),
-                ),
-              ),
-              SizedBox(width: 20),
-              Container(
-                width: 180,
-                height: 50,
-                child: DropdownButtonFormField<String>(
-                  value: _selectedCurrency,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _selectedCurrency = newValue!;
-                    });
-                  },
-                  items: ['USD', 'EUR', 'JPY', 'GBP'] // 선택 가능한 통화 목록
-                      .map((currency) {
-                    return DropdownMenuItem<String>(
-                      value: currency,
-                      child: Text(currency),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 180,
-                height: 50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: '검색어',
-                  ),
-                ),
-              ),
-              SizedBox(width: 20),
-              Container(
-                width: 180,
-                height: 50,
-                child: DropdownButtonFormField<String>(
-                  value: _selectedCurrency,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _selectedCurrency = newValue!;
-                    });
-                  },
-                  items: ['USD', 'EUR', 'JPY', 'GBP'] // 선택 가능한 통화 목록
-                      .map((currency) {
-                    return DropdownMenuItem<String>(
-                      value: currency,
-                      child: Text(currency),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
